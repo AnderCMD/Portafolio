c69427925f3eb91f35282167237b89e3f64884ec
AnderCMD <andercmd@outlook.com>
2024-10-25 10:48:39 -0600

    Actualizacion para Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  i18n: {
-    locales: ['es', 'en'],
-    defaultLocale: 'es',
-    routing: {
-      prefixDefaultLocale: true,
-      redirectToDefaultLocale: true
-    },
-  }
+	site: 'https://andercmd.github.io/Portafolio', // URL de tu página en GitHub Pages
+	base: '/Portafolio/', // Nombre del repositorio como base
+	integrations: [tailwind(), react()],
+	i18n: {
+		locales: ['es', 'en'],
+		defaultLocale: 'es',
+		routing: {
+			prefixDefaultLocale: true,
+			redirectToDefaultLocale: true,
+		},
+	},
 });
