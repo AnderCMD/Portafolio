a372a5142d9453cd3ec40fc7f5842c1200584b40
AnderCMD <andercmd@outlook.com>
2024-10-25 12:23:27 -0600

    Quitar Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://andercmd.dev', // URL de tu página en GitHub Pages
-	base: '/', // Nombre del repositorio como base
 	integrations: [tailwind(), react()],
 	i18n: {
 		locales: ['es', 'en'],
