55e5e9feaa491af9db1bc26346b8c0142106dc75
AnderCMD <andercmd@outlook.com>
2025-02-09 12:15:24 -0600

    Agregar soporte para Tailwind CSS v4 y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	vite: {
+		plugins:[tailwindcss()]
+	},
+	integrations: [react()],
 	i18n: {
 		defaultLocale: 'es',
 		locales: ['es', 'en'],
